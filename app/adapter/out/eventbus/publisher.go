7dfc93ee57c4521d8d87541c3a67191f71fc3bf5
ignaciojeria <ignaciovl.j@gmail.com>
2026-03-01 17:40:08 -0300

    publisher added

---
+++ app/adapter/out/eventbus/publisher.go
@@ -0,0 +1,33 @@
+package eventbus
+
+import (
+	"context"
+	"fmt"
+
+	"archetype/app/shared/infrastructure/eventbus"
+
+	"github.com/Ignaciojeria/ioc"
+)
+
+var _ = ioc.Register(NewTemplatePublisher)
+
+type TemplatePublisher struct {
+	publisher eventbus.Publisher
+}
+
+func NewTemplatePublisher(publisher eventbus.Publisher) (*TemplatePublisher, error) {
+	if publisher == nil {
+		return nil, fmt.Errorf("publisher dependency is nil")
+	}
+	return &TemplatePublisher{
+		publisher: publisher,
+	}, nil
+}
+
+func (p *TemplatePublisher) Publish(ctx context.Context, e eventbus.DomainEvent) error {
+	request := eventbus.PublishRequest{
+		Topic: "your-topic-name",
+		Event: e,
+	}
+	return p.publisher.Publish(ctx, request)
+}
